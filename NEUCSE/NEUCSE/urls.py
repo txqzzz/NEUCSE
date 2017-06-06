bb8aa1777742caa50a3e4ed3d9fe32b76083fbde
HellooYing <15642330153@163.com>
2017-06-06 23:00:44 +0800

    fetch

---
+++ NEUCSE/NEUCSE/urls.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""NEUCSE URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from treeView import views
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    #url(r'^treeView/', views.tree, name='tree'),
+    url(r'^treeView/', include('treeView.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'newspush/', include('NewsPush.urls', namespace='NewsPush')),
+    url(r'^online/',include('online.urls', namespace='online')),
+#    url(r'^组织结构.html', views., name='index'),
+#    url(r'^$/组织结构.html/', views.institude, name='institude'),
+]
---
+++ NEUCSE/README.md
@@ -0,0 +1,22 @@
+# NEUCSE treeView Test Project
+
+## Introduction
+This is one project based on [django-mptt] (https://django-mptt.github.io/django-mptt/index.html)for testing and developing treeView function of NEUCSE website.
+
+## Features
+* Add/delete nodes of the treeList
+* Edit the leaves' content
+
+## Environment
+* Python2.7+
+* django1.11
+
+## Requirements
+* ``django-mptt``
+
+## User-Guide
+* You should deploy the project locally first, [this may help] (https://docs.djangoproject.com/en/1.11/)
+* Manage urls: ``localhost:port(8000 default)/admin/``
+
+
+
